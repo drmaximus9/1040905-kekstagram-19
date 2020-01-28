b7bb32b894b26dfcd03910004560186316c1a9e2
Maxim <drmaximus9@gmail.com>
2020-01-28 14:27:36 +0300

    Исправления

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ var otherPictures = document.querySelector('.pictures');
 
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-var USER_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var USER_MESSAGES = [
+  'Всё отлично!', 'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
 var USER_NAMES = ['Васисуалий', 'Алехандро', 'Пипито', 'Джанго', 'Эркюль', 'Агата'];
 
@@ -64,151 +66,151 @@ var USER_COMMENTS = [
 
 var userPhotos = [
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
   },
   {
-    url: 'photos/' + getRandNumber(1, 25) + '.jpg' ,
+    url: 'photos/' + getRandNumber(1, 25) + '.jpg',
     description: '',
     likes: getRandNumber(15, 200),
     comments: getRandElement(USER_COMMENTS)
